66b38b49a0db2e1eedd878a95f727bd0a64a3a8b
Amritesh <54923120+ammrit2312@users.noreply.github.com>
2020-03-26 01:42:52 +0530

    Made the whole thing into one function
    the whole line will be printed now.

--- printf_to_printf.c
+++ printf_to_printf.c
@@ -1,27 +1,40 @@
 #include<stdio.h>
+#include<string.h>
+void convert(char code[],char code2[],char check[]);
+char printc[20]="printf",elifc[10]="else if",ifc[4]="if";
 int main(){
-       char code[90],code2[90],extr[100], print[20]="printf";
+       char code[90],code2[90],extr[100];
        printf("Enter your line of code: ");
        scanf("%[^\n]",code);
        //char *p=print;
        //printf("%p",p);
-       int k = 0;
-       int i=0, r=0,s=0,n=0;
-       for(i; code[i]!='\0'; i++){
-               if(code[i]==print[i]){
-                       code2[i]=print[i];                          
-               }
-               if(i>=5){
-                       code2[i]=code[i];
-               }
-                int j = 0;
-                for(j; code2[j]!='\0'; j++);
-                if(j==6){
-                        code2[i]='(';
-                        //printf("%s\n",code2);
-                        i=i+1;
-                        //printf("%d\n",i); 
-                        }
+    convert(code,code2,printc);
        }
-       printf("%s\n",code2);
-       }
+
+void convert(char code[],char code2[],char check[]){
+    int i=0;// r=0,s=0,n=0;
+    for(i; code[i]!='\0'; i++){
+            if(code[i]==check[i]){
+                    code2[i]=check[i];
+            }
+            if(i>=strlen(check)){
+                    code2[i]=code[i];
+            }
+             int j = 0;
+             for(j; code2[j]!='\0'; j++);
+             if(j==strlen(check)){//but why 6
+                     code2[i]='\0';
+                     //printf("%s\n",code2);
+                     i=i+1;
+                     //printf("%d\n",i);
+                     }
+    }
+    int k=0;
+    for(k; code[k]!='('; k++);
+    for(k;code[k]!='\0';k++){
+        code2[k-1]=code[k];
+    }
+    code2[k-1]='\0';
+    printf("%s\n%d\n",code2,k);//strlen(code2)=22
+}
+
