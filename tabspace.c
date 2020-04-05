d479d0ef0fb8fcf0b0f8f9c21eea9304b6da273c
sudhanvarajesh <62029635+sudhanvarajesh@users.noreply.github.com>
2020-04-05 21:52:41 +0530

    Update tabspace.c

--- tabspace.c
+++ tabspace.c
@@ -1,33 +1,33 @@
-void tab(char code[200])
+void tab(char code2[200])
 {
 
   int i=0,j=0,k=0,l=0;
-  for(i;code[i]!='\0';i++)
+  for(i;code2[i]!='\0';i++)
     { 
-      if(code[i-1]=='\n')
+      if(code2[i-1]=='\n')
         {
           for(l=0;l<k;l++)
             {
-              code2[j]='\t';
+              codefinal[j]='\t';
               j++;
             }
             
         }
-      if(code[i]=='{')
+      if(code2[i]=='{')
         {
-          code2[j]='\t';
+          codefinal[j]='\t';
           k++;
           j++;
         }
-      else if(code[i]=='}')
+      else if(code2[i]=='}')
         {
           k--;
         } 
       else
         {
-          code2[j]=code[i];
+          codefinal[j]=code2[i];
           j++;
         }  
     }
-  code2[j]='\0';
-} 
+  codefinal[j]='\0';
+}
