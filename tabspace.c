315a5e0119b3c26f080eae2e9fbeff70ef3d7711
sudhanvarajesh <62029635+sudhanvarajesh@users.noreply.github.com>
2020-04-05 21:21:15 +0530

    Update tabspace.c

--- tabspace.c
+++ tabspace.c
@@ -1,36 +1,33 @@
-#include <stdio.h>
-
-char tab(char cin[100])
+void tab(char code[200])
 {
 
-  char cout[120];
   int i=0,j=0,k=0,l=0;
-  for(i;cin[i]!='\0';i++)
+  for(i;code[i]!='\0';i++)
     { 
-      if(cin[i-1]=='\n')
+      if(code[i-1]=='\n')
         {
           for(l=0;l<k;l++)
             {
-              cout[j]='\t';
+              code2[j]='\t';
               j++;
             }
             
         }
-      if(cin[i]=='{')
+      if(code[i]=='{')
         {
-          cout[j]='\t';
+          code2[j]='\t';
           k++;
           j++;
         }
-      else if(cin[i]=='}')
+      else if(code[i]=='}')
         {
           k--;
         } 
       else
         {
-          cout[j]=cin[i];
+          code2[j]=code[i];
           j++;
         }  
     }
-  return cout;
+  code2[j]='\0';
 } 
