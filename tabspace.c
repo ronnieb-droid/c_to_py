bb0ae45ed780177aba468a9e78d8bfe6c67ac9db
sudhanvarajesh <62029635+sudhanvarajesh@users.noreply.github.com>
2020-04-05 23:10:13 +0530

    Update tabspace.c

--- tabspace.c
+++ tabspace.c
@@ -4,7 +4,7 @@ void tab(char code2[200])
   int i=0,j=0,k=0,l=0;
   for(i;code2[i]!='\0';i++)
     {
-      if(code2[i-1]=='\n')
+      if(code2[i-1]=='\n'&&code2[i-2]!='{')
         {
           for(l=0;l<k;l++)
             {
